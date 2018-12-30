9947d2f5f064745330b82a81ccfdd1e7361778f8
gbox3d <gbox3d@gmail.com>
2018-12-30 21:48:18 +0900

    exam 5,6 추가

---
+++ basic/ex05_accessPixel.py
@@ -0,0 +1,32 @@
+import numpy as np
+import cv2 as cv
+
+img = cv.imread('../messi5.jpg')
+px = img[100,100]
+print(px)
+
+#read pixel
+print(px[0])
+print(px[1])
+print(px[2])
+
+print(img[100,100,0])
+print(img[100,100,1])
+print(img[100,100,2])
+
+# get image info
+print(img.shape)
+print(img.size)
+print(img.dtype)
+
+#write pixel
+
+for x in range(100) :
+    for y in range(50) :
+        img[100 + y,100+x] = [0,0,255]
+
+#save result
+cv.imwrite('../output/test.png',img)
+
+
+
