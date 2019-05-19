5d5a8672b089f035434ac061323c422754f2acd1
gbox3d <gbox3d@gmail.com>
2019-05-19 22:28:12 +0900

    ...

---
+++ 1.py
@@ -0,0 +1,16 @@
+import numpy as np
+import cv2 as cv
+# Load an color image in grayscale
+img = cv.imread('messi5.jpg',0) 
+cv.namedWindow('image',cv.WINDOW_NORMAL)
+cv.imshow('image',img)
+# cv.waitKey(0)
+
+k = cv.waitKey(0)
+if k == 27:         # wait for ESC key to exit
+    cv.destroyAllWindows()
+elif k == ord('s'): # wait for 's' key to save and exit
+    cv.imwrite('messigray.png',img)
+    cv.destroyAllWindows()
+
+cv.destroyAllWindows()
